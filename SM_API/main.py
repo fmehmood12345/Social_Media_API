72335d9df2405ef28691af9940adf958598d8289
fmehmood12345 <mehmood.far@gmail.com>
2024-07-07 18:36:29 +0100

    added a get decorator into the main file and added 2 types of requirements files: requirements.txt and requirements-dev.txt

---
+++ SM_API/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI()  # fastAPI app
+
+
+# Get decorator - if a client makes a request to the / endpoint, then the return of the root function will be returned
+@app.get("/")
+async def root():  # async def means this function can run more or less at the same time as other functions
+    return {"message": "Hello, world!"}
