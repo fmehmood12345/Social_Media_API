d620dcca672cb30e74181af3e839d888b55a0cec
fmehmood12345 <mehmood.far@gmail.com>
2024-07-23 19:22:37 +0100

    made working get and post requests - done through insomnia.

--- SM_API/main.py
+++ SM_API/main.py
@@ -1,10 +1,43 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-
+from pydantic import BaseModel  # BaseModel is a class used to define a model. A model is used to validate data
+from typing import List
 app = FastAPI()  # fastAPI app
 
+"""Models used to validate incoming and outgoing data"""
 
-# Get decorator - if a client makes a request to the / endpoint, then the return of the root function will be returned
-@app.get("/")
-async def root():  # async def means this function can run more or less at the same time as other functions
-    return {"message": "Hello, world!"}
+
+class UserPostIn(BaseModel):  # Inheriting form BaseModel
+
+    # Defining fields in this model
+    body: str
+
+
+# Inheriting body field in the UserPost Class. UserPost class is used to give each post a unique identifier
+class UserPost(UserPostIn):
+    id: int
+
+
+"""Database"""
+post_table = {}
+
+
+# Create post endpoint
+@app.post("/post", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    # data = dict(post)
+    data = post.dict()  # defined structure of body
+    last_record_id = len(post_table)  # defined post id
+    new_post = {**data,
+                "id": last_record_id}  # Creates a new dictionary with all the filed and values of the dictionary data and contain their relative ids
+    post_table[last_record_id] = new_post
+    return new_post
+
+
+@app.get("/post", response_model=List[UserPost])  # Use List from typing
+async def get_all_posts():
+    return list(post_table.values())
+
+# # Get decorator - if a client makes a request to the / endpoint, then the return of the root function will be returned
+# @app.get("/")
+# async def root():  # async def means this function can run more or less at the same time as other functions
+#     return {"message": "Hello, world!"}
